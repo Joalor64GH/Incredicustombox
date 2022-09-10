5745ab12c76b737b0aad93ba49864096e422a726
Joalor64 <rhgarcon@gmail.com>
2022-09-10 10:27:27 -0400

    funni v2 icon colors

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -2,83 +2,83 @@
 var app = new function() {
     this.name = "Little Miss", this.version = "2", this.date = "2012", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 10, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
         name: "beat1_boom",
-        color: "828282",
+        color: "008200",
         uniqsnd: !1
     }, {
         name: "beat2_kashi",
-        color: "828282",
+        color: "289600",
         uniqsnd: !1
     }, {
         name: "beat3_paomeu",
-        color: "828282",
+        color: "50aa00",
         uniqsnd: !0
     }, {
         name: "beat4_ptttpeu",
-        color: "828282",
+        color: "78be00",
         uniqsnd: !1
     }, {
         name: "beat5_slupttt",
-        color: "828282",
+        color: "a0d200",
         uniqsnd: !0
     }, {
         name: "effet1_poulll",
-        color: "828282",
+        color: "ff0000",
         uniqsnd: !1
     }, {
         name: "effet2_tucati",
-        color: "828282",
+        color: "ff2800",
         uniqsnd: !0
     }, {
         name: "effet3_tuilopta",
-        color: "828282",
+        color: "ff5000",
         uniqsnd: !1
     }, {
         name: "effet4_tululou",
-        color: "828282",
+        color: "ff7800",
         uniqsnd: !0
     }, {
         name: "effet5_tumttt",
-        color: "828282",
+        color: "ffa000",
         uniqsnd: !1
     }, {
         name: "melo1_nananana",
-        color: "828282",
+        color: "1478aa",
         uniqsnd: !1
     }, {
         name: "melo2_pelulu",
-        color: "828282",
+        color: "288cbe",
         uniqsnd: !1
     }, {
         name: "melo3_siffle",
-        color: "828282",
+        color: "3ca0d2",
         uniqsnd: !1
     }, {
         name: "melo4_tatouti",
-        color: "828282",
+        color: "50b4e6",
         uniqsnd: !1
     }, {
         name: "melo5_tvutvutvu",
-        color: "828282",
+        color: "64c8fa",
         uniqsnd: !1
     }, {
         name: "coeur1_oaaah",
-        color: "828282",
+        color: "870078",
         uniqsnd: !1
     }, {
         name: "coeur2_cougou",
-        color: "828282",
+        color: "9b2896",
         uniqsnd: !1
     }, {
         name: "coeur3_porticoeur",
-        color: "828282",
+        color: "af50b4",
         uniqsnd: !1
     }, {
         name: "voix1_isit",
-        color: "828282",
+        color: "d7286e",
         uniqsnd: !1
     }, {
         name: "voix2_uare",
-        color: "828282",
+        color: "ff5096",
         uniqsnd: !1
     }], this.bonusarray = [{
         name: "Satisfied",
